6f732dc1a978e795351fc524ce650001466466f1
Lunga Phakathi <lungaphakathi93@gmail.com>
2024-04-22 12:09:57 +0200

    added products

---
+++ src/app/shared/models/Clothes.ts
@@ -0,0 +1,14 @@
+export class Clothes {
+    id!: string;
+    name!: string;
+    price!: number;
+    tags?: string[];
+    favorite!: boolean;
+    stars!: number;
+    imageUrl!: string;
+    brand!: string;
+    sizes!: string[];
+    material!: string;
+    color!: string;
+  }
+  
